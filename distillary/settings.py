c4fa2a1b68a6de7f18c237e3cd9a8489cd4f875e
StillsSma <samdawson301@live.com>
2017-02-17 15:17:46 -0500

    here we go

--- distillary/settings.py
+++ distillary/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9+bbhq&hm2yrp39jlzy!p8y54j=za9t-&=d55bf%vz5$bja9$8'
+SECRET_KEY = os.environ['SECRET_KEY_PORTFOLIO']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
